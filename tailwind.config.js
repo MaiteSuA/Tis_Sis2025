5acb1dbdec7cc9acaf525c721127f4dc4f78ef65
andreashirleygc@gmail.com <andreashirleygc@gmail.com>
2025-11-07 12:32:46 -0400

    Fix Merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindcss from '@tailwindcss/vite'
 
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
-  plugins: [react(), tailwindcss()],
+  plugins: [],
 };
