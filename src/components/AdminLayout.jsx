d5d90b67527615b5ba8cf001167e9c241c92f0ca
Carla Villarroel <aslecarmy@gmail.com>
2025-11-24 00:10:41 -0400

    arreglos visules

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -82,7 +82,7 @@ export default function AdminLayout({ children }) {
                 }`
               }
             >
-              Log
+              
             </NavLink>
           </nav>
         </div>
